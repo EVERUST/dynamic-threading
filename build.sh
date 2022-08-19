821d7270c398b7436a67402484aa59208a76adf2
euncharm1ng <applethrubottle@gmail.com>
2022-08-19 14:09:24 +0900

    buggy scenario added

--- build.sh
+++ build.sh
@@ -25,18 +25,16 @@ make
 for entry in *.ll
 do
 	echo $entry
-	# $LLVM/bin/opt -o target_rse_$entry -load-pass-plugin ./libpass.so -passes=RSE_pass $entry
+	$LLVM/bin/opt -o target_rse_$entry -load-pass-plugin ./libpass.so -passes=RSE_pass $entry
 	$LLVM/bin/opt -o target_tle_$entry -load-pass-plugin ./libpass.so -passes=TLE_pass $entry
 done
 
-# rustc --crate-type=lib --emit=obj -o probe_rse.o ../probe/probe_RSE.rs
+rustc --crate-type=lib --emit=obj -o probe_rse.o ../probe/probe_RSE.rs
 rustc --crate-type=lib --emit=obj -o probe_err.o ../probe/probe_ERR.rs
 
 #fd
-# $LLVM/bin/clang -o rse /home/uja/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-91db243dd05c003b.so -lpthread -pthread -lrt -lm target_rse_*.ll probe_rse.o ../include/libjemalloc_pic.a -ldl
+$LLVM/bin/clang -o rse /home/uja/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-91db243dd05c003b.so -lpthread -pthread -lrt -lm target_rse_*.ll probe_rse.o ../include/libjemalloc_pic.a -ldl
 $LLVM/bin/clang -o ../TLE/tle /home/uja/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-91db243dd05c003b.so -lpthread -pthread -lrt -lm target_tle_*.ll probe_err.o ../include/libjemalloc_pic.a -ldl
-#read_none
-#$LLVM/bin/clang -o rse /home/uja/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/libstd-91db243dd05c003b.so -lpthread -pthread -lrt -lm target_rse_*.ll probe_rse.o 
 
 rm *.ll
 
